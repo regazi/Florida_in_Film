047de4d415d6acfa910fbc03fedfd97cfad0c33f
Kenny J <102876557+regazi@users.noreply.github.com>
2022-06-08 17:46:44 -0400

    Project setup

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ http.createServer((request, response) => {
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
-    response.end(data);
+    response.end();
 
   });
 
