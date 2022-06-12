7701e125bcf9dd3c5e4270a02006e9991c208fa8
Kenny J <102876557+regazi@users.noreply.github.com>
2022-06-12 17:40:01 -0400

    /movie request handler added

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//movie repo
+
+let movieRepo = [
+    {
+      title: "Out Of Time",
+      filmingLocations: ["Naples", "Boca Grande", "Sarasota"]
+    },
+    {
+        title: "The Truman Show",
+        filmingLocations: ["Seaside"]
+    },
+    {
+      title: 'Ruby In Paradise',
+      filmingLocations: ["Panama City Beach"]
+    },
+    {
+      title: 'Gone Fishing',
+      filmingLocations: ["Naples", "Marco Island"]
+    }
+  ];
 //set up logging to write to log.txt
 
 fs = require("fs");
@@ -11,12 +31,14 @@ path = require("path");
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {flags: "a"});
 app.use(morgan("combined", {stream: accessLogStream}));
 
-
 //URL req handling
 
 app.use(express.static('public'));
 app.get("/", (req, res) => {
     res.send("Welcome to Florida in Film!");
+  });
+app.get("/movies", (req, res) => {
+    res.send(movieRepo);
   });
 app.use((err, req, res, next) => {
     console.error(err.stack);
