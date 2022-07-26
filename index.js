2d127bc3c922a87241bceffb2f30286dc4918dc7
Kenny J <102876557+regazi@users.noreply.github.com>
2022-07-26 14:17:59 -0400

    cors test

--- index.js
+++ index.js
@@ -21,20 +21,20 @@ const { check, validationResult } = require('express-validator');
 const cors = require('cors');
 app.use(cors());
 
-/*
+
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
+      return callback(new Error(message), false);
     }
     return callback(null, true);
   }
 }));
-*/
+
 
 //set up Passport 
 let auth = require('./auth')(app);
