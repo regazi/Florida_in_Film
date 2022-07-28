3984085147a9d2f40adfafee02e60a0a046c8aa0
Kenny J <102876557+regazi@users.noreply.github.com>
2022-07-28 10:53:42 -0400

    cors test

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const cors = require('cors');
 app.use(cors());
 
 
-let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:1234', 'http://m.media-amazon.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
       return callback(new Error(message), false);
     }
     return callback(null, true);
