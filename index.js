cb1395b00c331ea155e66ac5692d46fc180a837b
Kenny J <102876557+regazi@users.noreply.github.com>
2022-08-01 12:11:57 -0400

    added new rout for single user request

--- index.js
+++ index.js
@@ -74,6 +74,16 @@ app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) =
       res.status(500).send('Error: ' + err);
     });
 });
+app.get('/users/:username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 
 //create -- create new user
